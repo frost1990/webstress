7153da8b77101f5b86a6a7844f4cc5d5805f67b5
chensui <chensui@jd.com>
2015-11-05 13:03:57 +0800

    remove banner

--- src/main.c
+++ src/main.c
@@ -20,7 +20,6 @@ int main(int argc, char* argv[])
 	signal(SIGPIPE, SIG_IGN);
 	signal(SIGINT, interupt_summary);
 	SCREEN(SCREEN_GREEN, stdout, "Webstress - A simple web benchmarking tool\nCopyright (c) 2015 frost, simple1115@gmail.com. All rights deserved.\n\n");
-	SCREEN(SCREEN_BLUE, stdout, "This product is under development for the moment, please wait for a while.\nThank you for your support.\n\n");
 
 	gettimeofday(&start, NULL);
 	init_http_request(&myreq);
